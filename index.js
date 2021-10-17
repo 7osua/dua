e9c87ba9eb6607b845d8ea1ba9e50d4e3f5297f7
Josua Manullang <j7.manullang@gmail.com>
2021-10-17 10:09:59 +0700

    [^] fix p.write jadi p.textContent

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const h1 = document.createElement('h1');
 const p = document.createElement('p');
 
 h1.textContent = `Budget >= Pengeluaran`;
-p.write = `Setiap traveler perlu memastikan keamanan dan kenyamanan selama berpergian, termasuk dalam proses budgeting untuk memastikan pengeluaran traveling. Sehingga keuangan tetap terjaga dan dapat diatur. 
+p.textContent = `Setiap traveler perlu memastikan keamanan dan kenyamanan selama berpergian, termasuk dalam proses budgeting untuk memastikan pengeluaran traveling. Sehingga keuangan tetap terjaga dan dapat diatur. 
 
 Web ini akan membantu traveler mempersiapkan daftar pengeluaran yang diperlukan, mulai dari yang pengeluaran yang sangat penting hingga mempersiapkan untuk pengeluaran yang sangat mendesak.
 
