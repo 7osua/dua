b9c0d36d16e4e4302ae0f663f53cc789f9f885f3
Josua Manullang <j7.manullang@gmail.com>
2021-10-24 06:56:03 +0700

    [^] branch untuk development

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const root = document.getElementById('root');
 const section = document.createElement('section');
 const h1 = document.createElement('h1');
 
-h1.textContent = 'On load . . .';
+h1.textContent = 'Comming Soon . . .';
 section.append(h1);
 root.append(section);
