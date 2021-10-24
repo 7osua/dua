55aa6e94aff70bdb23132456534c4b34e707543b
Josua Manullang <j7.manullang@gmail.com>
2021-10-24 16:55:58 +0700

    [^] membuat initial content nav+main+footer

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 const root = document.getElementById('root');
-const section = document.createElement('section');
-const h1 = document.createElement('h1');
 
-h1.textContent = 'Comming Soon . . .';
-section.append(h1);
-root.append(section);
+const navigation = () => {
+    root.insertAdjacentHTML(
+        'beforebegin',
+        `
+        <nav>
+            <div>
+                <a>
+                    02
+                </a>
+            </div>
+            <ul>
+                <li><a href="#input">Input</a></li>
+                <li><a href="#process">Hitung</a></li>
+                <li><a href="#output">Output</a></li>
+            </ul>
+        </nav>
+        `
+    );
+};
+
+const main = () => {
+    root.insertAdjacentHTML(
+        'beforebegin',
+        `
+        <main>
+            <section>
+                <h1>Travel Budgetting Harusnya </br> Menjadi Lebih Mudah.</h1>
+            </section>
+        </main>
+        `
+    );
+};
+
+const footer = () => {
+    root.insertAdjacentHTML(
+        'afterend',
+        `
+        <footer>
+            <ul>
+                <li><a>josua!</a></li>
+                <li><a>GitHub</a></li>
+                <li><a>LinkeIn</a></li>
+                <li><a>Twitter</a></li>
+                <li><a>Instagram</a></li>
+            <ul>
+        </footer>
+        `
+    );
+};
+
+const initPage = () => {
+    navigation();
+    main();
+    footer();
+};
+
+window.onload(initPage());
