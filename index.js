1a81792365311c4ac4933d3da94261fbb0cba529
Josua Manullang <j7.manullang@gmail.com>
2021-10-28 22:12:41 +0700

    [^] mulai membuat component ui dengan javascript

--- index.js
+++ index.js
@@ -1,93 +1,23 @@
 const root = document.getElementById('root');
 
-const navigation = () => {
+const header = () => {
     root.insertAdjacentHTML(
         'beforebegin',
         `
-        <nav>
-            <div>
-                <a>
-                    02
-                </a>
-            </div>
-            <ul>
-                <li><a href="#input" id="input">Input</a></li>
-                <li><a href="#process" id="process">Hitung</a></li>
-                <li><a href="#output" id="output">Output</a></li>
-            </ul>
-        </nav>
-        `
-    );
-};
-
-const main = () => {
-    root.insertAdjacentHTML(
-        'afterbegin',
-        `
-        <main>
-            <section>
-                <h1>Travel Budgetting Harusnya </br> Menjadi Lebih Mudah.</h1>
-            </section>
-        </main>
-        `
-    );
-};
-
-const footer = () => {
-    root.insertAdjacentHTML(
-        'afterend',
-        `
-        <footer>
-            <ul>
-                <li><a>josua!</a></li>
-                <li><a>GitHub</a></li>
-                <li><a>LinkeIn</a></li>
-                <li><a>Twitter</a></li>
-                <li><a>Instagram</a></li>
-            <ul>
-        </footer>
-        `
+            <header class="head">
+                  <div class="head-logo">
+                        <img class="head-logo-image"
+                             src="favicon_io/android-chrome-192x192.png"
+                             alt="04.empat">
+                        <h1 class="head-logo-label">dua</h1>
+                  </div>
+            </header>
+        `,
     );
 };
 
 const initPage = () => {
-    navigation();
-    main();
-    footer();
-};
-
-const showSection = () => {
-    const mainSection = root.getElementsByTagName('section')[0];
-    const h2 = document.createElement('h2');
-    const sectionName = location.hash; 
-    locNow = location.hash;
-    console.log(locNow);
-
-    if (location.hash === '#input') {
-        h2.textContent = 'Ini Input';
-        mainSection.firstElementChild.replaceWith(h2);
-    } else if (location.hash === '#process') {
-        h2.textContent = 'Ini process';
-        mainSection.firstElementChild.replaceWith(h2);
-    } else {
-        h2.textContent = 'Ini output';
-        mainSection.firstElementChild.replaceWith(h2);
-    }
+    header();
 };
 
 window.onload = initPage();
-const inputSection = document.getElementById('input');
-const processSection = document.getElementById('process');
-const outputSection = document.getElementById('output');
-
-inputSection.onclick = function () {
-    showSection();
-};
-
-processSection.onclick = function () {
-    showSection();
-};
-
-outputSection.onclick = function () {
-    showSection();
-};
